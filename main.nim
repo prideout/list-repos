09dd93d5bed96dd4e3e9b38fb5b2fcf862851307
Philip Rideout <philiprideout@gmail.com>
2018-07-23 17:49:23 -0700

    Repair.

--- main.nim
+++ main.nim
@@ -6,14 +6,18 @@ import strformat
 
 # TODO: VS Code nim extension should support -d:ssl
 
-const url = "https://api.github.com/user/repos?per_page=100"
-
 let client = newHttpClient()
 
-# encodedAuth = b64encode("username:password").decode("ascii")
-# client.headers = newHttpHeaders({
-#     "Authorization": "Basic {encodedAuth}"
-# })
+const showPrivate = false
+
+when showPrivate:
+    const url = "https://api.github.com/user/repos?per_page=100"
+    encodedAuth = b64encode("username:password").decode("ascii")
+    client.headers = newHttpHeaders({
+        "Authorization": "Basic {encodedAuth}"
+    })
+else:
+    const url = "https://api.github.com/users/prideout/repos?per_page=100"
 
 let responseString = client.getContent(url)
 let dbJson = parseJson(responseString)
@@ -26,14 +30,15 @@ for repo in dbJson:
     if not archived and not private:    
         echo fmt"{name:>21} ... {desc:<72}"
 
-echo "\nprivate: "
-for repo in dbJson:
-    var (name, desc) = (repo["name"].str, repo["description"].str)
-    if desc == nil: desc = "NO DESCRIPTION"
-    let private = repo["private"].getBool()
-    let archived = repo["archived"].getBool()
-    if not archived and private:
-        echo fmt"{name:>21} ... {desc:<72}"
+when showPrivate:
+    echo "\nprivate: "
+    for repo in dbJson:
+        var (name, desc) = (repo["name"].str, repo["description"].str)
+        if desc == nil: desc = "NO DESCRIPTION"
+        let private = repo["private"].getBool()
+        let archived = repo["archived"].getBool()
+        if not archived and private:
+            echo fmt"{name:>21} ... {desc:<72}"
 
 write(stdout, "\narchived: ")
 for repo in dbJson:
